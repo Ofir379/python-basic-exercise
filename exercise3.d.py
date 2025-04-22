310582b345fb792affaafd4e1435c5ce702ceeb4
Ofir379 <ofiror2000@gmail.com>
2025-04-22 14:38:00 +0000

    lab saftey

---
+++ exercise3.d.py
@@ -0,0 +1,8 @@
+temperature = 25 
+pressure = 45  
+voltage = 220
+if 20 <= temperature <= 80 and pressure < 50 and 200 <= voltage <= 250:
+    print("Safe to proceed")
+else:
+    print("Unsafe conditions")
+
