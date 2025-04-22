06f4b53cc5a49f36b44e6e5a7c808c1551ac616e
Ofir379 <ofiror2000@gmail.com>
2025-04-22 14:50:18 +0000

    function

---
+++ function.exercise.py
@@ -0,0 +1,7 @@
+num = 10
+str_val = "Hello"
+bool_val = True
+
+print("The value of num is", num, "and its type is", type(num))
+print("The value of str is", str_val, "and its type is", type(str_val))
+print("The value of bool is", bool_val, "and its type is", type(bool_val))
