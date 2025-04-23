781ee5a16117ea135867a8933a28be7481ee19f9
Ofir379 <ofiror2000@gmail.com>
2025-04-23 13:49:34 +0000

    manual evalution and boolean expressions

---
+++ manualevalution.booleanexpressions.py
@@ -0,0 +1,14 @@
+PART 1
+1.true
+2.true
+3.false
+4.false
+5.true
+
+PART 2
+1. x <=20 => 10
+2. len(s) > 0 and "py" in 5
+3. n < 0 or abs(n) >100
+4. or (user_role == "admin" and active) superuser
+5. not ( temperature < 0 or temperature > 35 )
+
